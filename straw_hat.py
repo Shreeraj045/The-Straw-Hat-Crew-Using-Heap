777bcd6393e04aeb68ae3a704f751270c1afe0f6
shreeraj <shreeraj05jambhale@gmail.com>
2024-09-29 00:59:06 +0530

    continuation

--- straw_hat.py
+++ straw_hat.py
@@ -27,7 +27,9 @@ class StrawHatTreasury:
 
 
         self.number = m
-        self.crew_heap = Heap(self.compare_func1())
+        self.crew_heap = Heap(self.compare_func1,[])
+        for i in range(m):
+            self.crew_heap.insert(CrewMate())
         self.treasure_list = []
         self.last_time = 0
         self.sec_last_time = 0
@@ -36,7 +38,7 @@ class StrawHatTreasury:
     def compare_func1(self,crew1:CrewMate,crew2:CrewMate):
         return crew1.load < crew2.load
 
-    def add_treasure(self, treasure):
+    def add_treasure(self, treasure:Treasure):
         '''
         Arguments:
             treasure : Treasure : The treasure to be added to the treasury
@@ -50,11 +52,16 @@ class StrawHatTreasury:
                 n : Number of Treasures
         '''
         custom_treasure = treasure
+
+        print("times1- ",self.last_time , self.sec_last_time)
+        self.sec_last_time = self.last_time
+        self.last_time = treasure.arrival_time
         time_diff = self.last_time - self.sec_last_time
-        print(time_diff)
+        print("times2- ", self.last_time, self.sec_last_time)
+        print("time diff - ",time_diff)
         for i in self.crew_heap.init_array:
             i.load -= time_diff
-            if i.load <0 : i.load = 0
+            if i.load < 0 : i.load = 0
         self.treasure_list.append(custom_treasure)
         temp = self.crew_heap.extract()
         temp.add_treasure_in_crew(custom_treasure)
@@ -75,13 +82,70 @@ class StrawHatTreasury:
         '''
         # Write your code here
         for j in self.crew_heap.init_array :
-            i.just_completion_time()
-
+            j.just_completion_time()
         for i in self.treasure_list:
-            print(i)
+            print(i.completion_time,end="  ")
+        print()
     
     # You can add more methods if required
 
+
+
 #################   TESTING #################
 print("_"*20,"start testing","_"*20)
+
+# Initialize with 3 crew members
+straw = StrawHatTreasury(3)
+
+# 10 treasures with some having the same arrival time (collisions)
+treasure1 = Treasure(1001, 5, 1)  # Arrives at time 1
+straw.add_treasure(treasure1)
+print("load-", [i.load for i in straw.crew_heap.init_array])
+straw.get_completion_time()
+print("-"*50)
+
+treasure2 = Treasure(1002, 4, 2)  # Arrives at time 2
+straw.add_treasure(treasure2)
+print("load-", [i.load for i in straw.crew_heap.init_array])
+straw.get_completion_time()
+print("-"*50)
+
+treasure3 = Treasure(1003, 6, 3)  # Arrives at time 3
+straw.add_treasure(treasure3)
+print("load-", [i.load for i in straw.crew_heap.init_array])
+straw.get_completion_time()
+print("-"*50)
+
+# Collision at time 4: 3 treasures arrive at the same time
+treasure4 = Treasure(1004, 7, 4)  # Arrives at time 4
+straw.add_treasure(treasure4)
+print("load-", [i.load for i in straw.crew_heap.init_array])
+straw.get_completion_time()
+print("-"*50)
+
+# Additional treasures arriving in sequence
+treasure7 = Treasure(1007, 3, 5)  # Arrives at time 5
+straw.add_treasure(treasure7)
+print("load-", [i.load for i in straw.crew_heap.init_array])
+straw.get_completion_time()
+print("-"*50)
+
+treasure8 = Treasure(1008, 9, 6)  # Arrives at time 6
+straw.add_treasure(treasure8)
+print("load-", [i.load for i in straw.crew_heap.init_array])
+straw.get_completion_time()
+print("-"*50)
+
+treasure9 = Treasure(1009, 4, 7)  # Arrives at time 7
+straw.add_treasure(treasure9)
+print("load-", [i.load for i in straw.crew_heap.init_array])
+straw.get_completion_time()
+print("-"*50)
+
+treasure10 = Treasure(1010, 1, 8)  # Arrives at time 8
+straw.add_treasure(treasure10)
+print("load-", [i.load for i in straw.crew_heap.init_array])
+straw.get_completion_time()
+print("-"*50)
+
 
